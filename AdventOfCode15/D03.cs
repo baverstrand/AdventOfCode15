6694b395261037df5bb5eaceb5f5ba1a9e8358e0
Sofie Bäverstrand <sofie.baverstrand@gmail.com>
2023-09-22 16:55:11 +0200

    D03 Start

--- AdventOfCode15/D03.cs
+++ AdventOfCode15/D03.cs
@@ -12,10 +12,41 @@ namespace AdventOfCode15
     {
         public static void Run()
         {
+            // Läs in data
             var day = "03D";
             var data = Helpers.ReadText(day);
 
             #region part1
+
+            //Skapa grid och startposition
+            var grid = new List<CounterPoint>();
+            var xpos = 0;
+            var ypos = 0;
+            grid.Add(new CounterPoint { X = xpos, Y = ypos, Counter = 1 });
+
+            // Gå igenom pil för pil
+            foreach (char c in data)
+            {
+                if (c == '>')
+                {
+                    xpos++;
+                }
+                else if (c == '<')
+                {
+                    xpos--;
+                }
+                else if (c == '^')
+                {
+                    ypos--;
+                }
+                else
+                {
+                    ypos++;
+                }
+
+            }
+            // Kolla om koordinaten finns
+            // Om finns, plussa counter, annars skapa point 
           
             #endregion
 

1f3bf57321d4bd2b93c2af2ce7ea814c340e812c
Sofie Bäverstrand <sofie.baverstrand@gmail.com>
2023-09-26 11:27:32 +0200

    D05Start

---
+++ AdventOfCode15/D04.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode15
+{
+    public class D04
+    {
+        public static void Run()
+        {
+            var input = "bgvyzdsv";
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode15/D05.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode15
+{
+    public class D05
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}

2020ee8c5c154923153e999756b4021e2b1a68c2
Sofie Bäverstrand <sofie.baverstrand@gmail.com>
2023-09-26 14:59:37 +0200

    D05 forts

--- AdventOfCode15/D05.cs
+++ AdventOfCode15/D05.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode15.Help;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,69 @@ namespace AdventOfCode15
     {
         public static void Run()
         {
+            var day = "05D";
+            var data = Helpers.ReadLines(day);
 
+            var counter = 0;
+
+            foreach (var line in data)
+            {
+                //if (IsNiceVowels(line) && HasDoubleLetter(line) && NoForbiddenStrings(line))
+                //{
+                //    counter++;
+                //}
+            }
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+
+        private static bool HasPairs (string input)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                // substring 1 & 2, if .contains 1 return true
+            }
+            return false;
+        }
+
+        private static bool IsNiceVowels (string input)
+        {
+            var counter = 0;
+            var vowels = "aeiou";
+            foreach (var c in input)
+            {
+                if (vowels.Contains(c))
+                {
+                    counter++;
+                    if (counter == 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool HasDoubleLetter (string input)
+        {
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i +1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool NoForbiddenStrings (string input)
+        {
+            if (input.Contains("ab") || input.Contains("cd") || input.Contains("pq") || input.Contains("xy"))
+            {
+                return false;
+            }
+            return true;
         }
     }
 }

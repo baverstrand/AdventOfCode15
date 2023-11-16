e7354ce143d09903a53b5dbc4951d5b0f96b7ba5
Sofie Bäverstrand <sofie.baverstrand@gmail.com>
2023-11-16 14:10:28 +0100

    D10 start

---
+++ AdventOfCode15/D10.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode15.Help;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode15
+{
+    public class D10
+    {
+        /* 
+         * Look-and-say sequences are generated iteratively, using the previous value as input for the next step. 
+         * For each step, take the previous value, and replace each run of digits (like 111) with the number of 
+         * digits (3) followed by the digit itself (1).
+         */
+
+        public static void Run()
+        {
+            var input = "3113322113";
+        }
+    }
+}
